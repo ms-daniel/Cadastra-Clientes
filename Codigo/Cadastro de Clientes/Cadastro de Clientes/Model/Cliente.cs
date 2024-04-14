fa2788f930e28d7f537486393b3f6d3b018efbc4
Carlos Daniel <ms_danniel@outlook.com>
2024-04-14 10:43:53 -0300

    projeto principal

---
+++ Codigo/Cadastro de Clientes/Cadastro de Clientes/Model/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cadastro_de_Clientes.Model
+{
+    public class Cliente
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string logotipo { get; set; }
+
+    }
+}
