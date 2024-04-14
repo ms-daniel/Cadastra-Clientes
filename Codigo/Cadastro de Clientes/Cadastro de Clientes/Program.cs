0e170facec1d0ebfd7e36ac32079a4d1852aac32
Carlos Daniel <ms_danniel@outlook.com>
2024-04-14 20:07:06 -0300

    injeção

--- Codigo/Cadastro de Clientes/Cadastro de Clientes/Program.cs
+++ Codigo/Cadastro de Clientes/Cadastro de Clientes/Program.cs
@@ -1,3 +1,6 @@
+using Core.Service;
+using Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//injection
+builder.Services.AddTransient<IClienteService, ClienteService>();
+builder.Services.AddTransient<ILogradouroService, LogradouroService>();
+
 
 var app = builder.Build();
 
