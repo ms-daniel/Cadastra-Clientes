acc37355cacba2ddeb044ce6ef80270100b58717
Carlos Daniel <ms_danniel@outlook.com>
2024-04-14 10:59:24 -0300

    interfaces de service

---
+++ Codigo/Cadastro de Clientes/Core/Service/IClienteService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IClienteService
+    {
+        int Create(Cliente cliente);
+        void Edit(Cliente cliente);
+        void Delete(int idCliente);
+        Cliente Get(int idCliente);
+
+        IEnumerable<Cliente> GetAll();
+    }
+}
---
+++ Codigo/Cadastro de Clientes/Core/Service/ILogradouroService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface ILogradouroService
+    {
+        int Create(Logradouro logradouro);
+        void Edit(Logradouro logradouro);
+        void Delete(int idLogradouro);
+        Logradouro Get(int idLogradouro);
+
+        IEnumerable<Logradouro> GetAll();
+    }
+}
