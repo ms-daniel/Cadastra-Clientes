5b2e78075cc93e95544fd074909dcbea77b1dcfe
Carlos Daniel <ms_danniel@outlook.com>
2024-04-14 11:38:33 -0300

    projeto service

---
+++ Codigo/Cadastro de Clientes/Service/ClienteService.cs
@@ -0,0 +1,45 @@
+﻿using Core;
+using Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class ClienteService : IClienteService
+    {
+        private readonly DesafioCadastroContext _context;
+
+        public ClienteService(DesafioCadastroContext context)
+        {
+            _context = context;
+        }
+
+        public int Create(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int idCliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente Get(int idCliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cliente> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Codigo/Cadastro de Clientes/Service/LogradouroService.cs
@@ -0,0 +1,45 @@
+﻿using Core;
+using Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    internal class LogradouroService : ILogradouroService
+    {
+        private readonly DesafioCadastroContext _context;
+
+        public LogradouroService (DesafioCadastroContext context)
+        {
+            _context = context;
+        }
+
+        public int Create(Logradouro logradouro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int idLogradouro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Logradouro logradouro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Logradouro Get(int idLogradouro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Logradouro> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
