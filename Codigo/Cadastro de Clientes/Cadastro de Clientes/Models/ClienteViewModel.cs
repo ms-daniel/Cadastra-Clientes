ee93b40e5c4a60ec206d093c910a9dcbfe3fffc6
Carlos Daniel <ms_danniel@outlook.com>
2024-04-15 11:04:18 -0300

    name changed

---
+++ Codigo/Cadastro de Clientes/Cadastro de Clientes/Models/ClienteViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class ClienteViewModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Logotipo { get; set; }
+
+    }
+}
