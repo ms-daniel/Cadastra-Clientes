c286166b18b968d28ae5ea8db08317150c1747b8
Carlos Daniel <ms_danniel@outlook.com>
2024-04-15 11:05:29 -0300

    cliente controller Add e GetAll

---
+++ Codigo/Cadastro de Clientes/Cadastro de Clientes/Controllers/ClienteController.cs
@@ -0,0 +1,40 @@
+﻿using Models;
+using Core.Service;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Core;
+
+namespace Cadastro_de_Clientes.Controllers
+{
+    [ApiController]
+    [Route("api/v1/cliente")]
+    public class ClienteController : ControllerBase
+    {
+        private readonly IClienteService _clienteService;
+        IMapper _mapper;
+
+        public ClienteController(IClienteService clienteService, IMapper mapper)
+        {
+            _clienteService = clienteService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult Add(ClienteViewModel cliente)
+        {
+            var clienteCore = _mapper.Map<Cliente>(cliente);
+            _clienteService.Create(clienteCore);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var clientes = _clienteService.GetAll();
+            var clienteList = _mapper.Map<List<ClienteViewModel>>(clientes);
+            
+            return Ok(clienteList);
+        }
+    }
+}
