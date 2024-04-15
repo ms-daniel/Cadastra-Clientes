0d273bd3229eafeb1204740014af08ca07f1ee06
Carlos Daniel <ms_danniel@outlook.com>
2024-04-15 11:05:40 -0300

    mapper para cliente

---
+++ Codigo/Cadastro de Clientes/Cadastro de Clientes/Mappers/ClienteProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Models;
+using Core;
+
+namespace Mappers
+{
+    public class ClienteProfile : Profile 
+    {
+        public ClienteProfile()
+        {
+            CreateMap<ClienteViewModel, Cliente>().ReverseMap();
+        }
+    }
+}
