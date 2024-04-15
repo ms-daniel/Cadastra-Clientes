b6ca90202896840794ec80ca917cfaae9a2577f9
Carlos Daniel <ms_danniel@outlook.com>
2024-04-15 18:38:45 -0300

    auth controller

---
+++ Codigo/APICadastroClientes/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using Cadastro_de_Clientes.Services;
+using Microsoft.AspNetCore.Mvc;
+using Core;
+
+namespace Cadastro_de_Clientes.Controllers
+{
+    [ApiController]
+    [Route("api/v1/auth")]
+    public class AuthController : Controller
+    {
+        [HttpPost]
+        public IActionResult Auth(string username, string password)
+        {
+            if (username == "desafio" && password == "123456")
+            {
+                var token = TokenService.GenerateToken(new Cliente());
+                return Ok(token);
+            }
+
+            return BadRequest("Usuário ou senha inválido");
+        }
+    }
+}
