56f13e3baa84950e81813f5fd8344deee90efcf8
Carlos Daniel <ms_danniel@outlook.com>
2024-04-24 23:45:45 -0300

    loading component

---
+++ Codigo/cadastro_clientes_web_frontend/src/components/Loading.js
@@ -0,0 +1,8 @@
+import ReactLoading from 'react-loading';
+import React, { useState } from 'react'
+
+const LoadingComp = () => (
+    <ReactLoading type="spin" color="#0E4DC2" height={50} width={50} />
+);
+
+export default LoadingComp;
