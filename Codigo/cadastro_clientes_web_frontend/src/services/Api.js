b4cea2738174b72276a862122ae45a83fd308381
Carlos Daniel <ms_danniel@outlook.com>
2024-04-26 12:09:10 -0300

    metodo para getall de clientes

--- Codigo/cadastro_clientes_web_frontend/src/services/Api.js
+++ Codigo/cadastro_clientes_web_frontend/src/services/Api.js
@@ -6,25 +6,53 @@ const api = axios.create({
 });
 
 const logIn = ({usernameValue, passwordValue, setLoadComp}) => {
+  api
+    .post("/auth/", {
+      username: usernameValue,
+      password: passwordValue
+    })
+    .then((response) => {
+      //console.log('Resposta do servidor:', response.data);
+      localStorage.setItem('accessToken', response.data.token);
+      showToastMessage('success', 'Login successful!');
+      setLoadComp(false);
+    })
+    .catch((err) => {
+      if(err.response && err.response.data && err.response.data.fromServer){
+        showToastMessage('error', 'Incorrect username or password.');
+      } else {
+        showToastMessage('error', 'Some unknown error occurred.');
+      }
+      setLoadComp(false);
+    });
+};
+
+const getCustomers = ({pageNumber, pageQuantity}) => {
+  const token = localStorage.getItem('accessToken');
+
+  if (token){
     api
-      .post("/auth/", {
-        username: usernameValue,
-        password: passwordValue
-      })
-      .then((response) => {
-        console.log('Resposta do servidor:', response.data);
-        localStorage.setItem('accessToken', response.data.token);
-        showToastMessage('success', 'Login successful!');
-        setLoadComp(false);
-      })
-      .catch((err) => {
-        if(err.response && err.response.data && err.response.data.fromServer){
-          showToastMessage('error', 'Incorrect username or password.');
-        } else {
-          showToastMessage('error', 'Some unknown error occurred: ');
-        }
-        setLoadComp(false);
-      });
+    .post("/v1/client/getall", {
+      params: {
+        pageNumber: pageNumber,
+        pageQuantity: pageQuantity 
+      }
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      if(err.response && err.response.data && err.response.data.fromServer){
+        //showToastMessage('error', 'Incorrect username or password.');
+      } else {
+        showToastMessage('error', 'Some unknown error occurred.');
+      }
+      
+    });
+  } else {
+
+  }
+  
 };
 
 export {logIn};
