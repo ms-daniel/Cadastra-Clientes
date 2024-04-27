619f5664d5343b848ec511b93c4ebf6dcc91884b
Carlos Daniel <ms_danniel@outlook.com>
2024-04-27 12:12:44 -0300

    route para clientes

--- Codigo/cadastro_clientes_web_frontend/src/App.js
+++ Codigo/cadastro_clientes_web_frontend/src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Clients from './pages/Clients';
 import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false)
+  const [loggedIn, setLoggedIn] = useState(true)
 
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login setLoggedIn={setLoggedIn}/>} />
+          <Route path="/clients" element={<Clients loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>} />
           <Route path="/home" element={<Home loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}/>
         </Routes>
       </BrowserRouter>
