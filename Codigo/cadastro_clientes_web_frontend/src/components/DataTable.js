f8c72093d8855cc2d944baa4230de6b84c33558c
Carlos Daniel <ms_danniel@outlook.com>
2024-04-26 22:03:20 -0300

    ORGANIZACAO DO DATABLE

--- Codigo/cadastro_clientes_web_frontend/src/components/DataTable.js
+++ Codigo/cadastro_clientes_web_frontend/src/components/DataTable.js
@@ -3,34 +3,20 @@ import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-    },
+    { field: 'Name', headerName: 'Name', width: 130 },
+    { field: 'Email', headerName: 'Email', width: 130 }
   ];
   
   const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, Name: 'Snow', Email: 'Jon'},
+    { id: 2, Name: 'Lannister', Email: 'Cersei'},
+    { id: 3, Name: 'Lannister', Email: 'Jaime'},
+    { id: 4, Name: 'Stark', Email: 'Arya'},
+    { id: 5, Name: 'Targaryen', Email: 'Daenerys'},
+    { id: 6, Name: 'Melisandre', Email: null},
+    { id: 7, Name: 'Clifford', Email: 'Ferrara'},
+    { id: 8, Name: 'Frances', Email: 'Rossini'},
+    { id: 9, Name: 'Roxie', Email: 'Harvey'},
   ];
   
   export default function DataTable() {
