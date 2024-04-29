a69d51d9a9c06d95833de193c79298aee35fe11c
Carlos Daniel <ms_danniel@outlook.com>
2024-04-28 23:27:50 -0300

    removido toast de notificação

--- Codigo/cadastro_clientes_web_frontend/src/shared/Layout.js
+++ Codigo/cadastro_clientes_web_frontend/src/shared/Layout.js
@@ -2,12 +2,11 @@
 import React from 'react';
 import DrawerAppBar from '../components/HamburgerMenu';
 import Toolbar from '@mui/material/Toolbar';
-import { ToastContainer } from 'react-toastify';
 
 const Layout = ({ children }) => {
   return (
     <div className='container flex'>
-        <ToastContainer />
+        
         <div className='row'>
             <DrawerAppBar />
             <Toolbar />
