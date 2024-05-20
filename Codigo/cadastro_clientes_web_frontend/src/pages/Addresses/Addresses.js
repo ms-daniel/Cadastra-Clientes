7bd668b731d051071008a7882254559a7c7b0f7c
Carlos Daniel <ms_danniel@outlook.com>
2024-05-20 16:28:26 -0300

    index addresses

--- Codigo/cadastro_clientes_web_frontend/src/pages/Addresses/Addresses.js
+++ Codigo/cadastro_clientes_web_frontend/src/pages/Addresses/Addresses.js
@@ -1,5 +1,5 @@
-import React, { useState,useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState,useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import showToastMessage from '../../components/Notify';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DrawerAppBar from '../../components/HamburgerMenu';
@@ -11,9 +11,8 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
-import DataTable from '../../components/DataTable'
-import {ToastContainer} from "react-toastify";
-import {getClients} from '../../services/Api'
+import {DataTableAddress} from '../../components/DataTable';
+import {getAllAddresses, deleteAddress} from '../../services/AddressesActions';
 import Button from '@mui/material/Button';
 import PersonIcon from '@mui/icons-material/Person';
 import AddIcon from '@mui/icons-material/Add';
@@ -21,80 +20,46 @@ import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { red, blue } from '@mui/material/colors';
+import Layout from '../../shared/Layout';
+import { ModalDelete } from '../../components/Modals';
 
 
 
-const Clients = (props) => {
+const Addresses = (props) => {
     const navigate = useNavigate();
+    const location = useLocation();
 
-    const colAd = [
-        { field: 'id', headerName: 'ID', width: 70 , flex: 1},
-        { field: 'name', headerName: 'Name', width: 150, flex: 2 },
-        { field: 'email', headerName: 'Email', width: 200, flex: 3},
-        { field: 'addresses', headerName: 'Addresses', width: 90, flex: 1},
-        { 
-            field: 'actions',
-            headerName: 'Actions',
-            width: 250,
-            flex: 2 ,
-            sortable: false,
-            renderCell: (params) => (
-            <div>
-                <IconButton
-                aria-label="edit"
-                onClick={() => handleEdit(params.row.id)}
-                sx={{ color: blue['A700'] }} 
-                >
-                <EditIcon />
-                </IconButton>
-                <IconButton
-                aria-label="delete"
-                onClick={() => handleDelete(params.row.id)}
-                sx={{ color: red['A700'] }}
-                >
-                <DeleteIcon />
-                </IconButton>
-            </div>
-            ),
-        }
+    const colAddresses = [
+        { field: 'id', headerName: 'ID', width: 70 },
+        { field: 'clienteId', headerName: 'Client_ID', width: 70 },
+        { field: 'rua', headerName: 'Street', width: 250 },
+        { field: 'numero', headerName: 'Number', width: 70 },
+        { field: 'bairro', headerName: 'Neighborhood', width: 110 },
+        { field: 'cidade', headerName: 'City', width: 150 },
+        { field: 'estado', headerName: 'State', width: 70 },
+        { field: 'cep', headerName: 'Postal Code', width: 100 }
     ];
 
-    const [addresses, setAddresses] = useState([]);
-
-    const handleEdit = (id) => {
-        console.log('Editar item com ID:', id);
-      };
-    
-      const handleDelete = (id) => {
-        console.log('Excluir item com ID:', id);
-      };
+    const fetchAddresses = async (pgNumber, pgQt, orderby) => {
+        try {
+            const data = await getAllAddresses({ pageNumber: pgNumber, pageQuantity: pgQt, order: orderby });
+            return(data);
+        } catch (error) {
+            console.error('Error fetching clients:', error);
+            //showToastMessage('error', 'Failed to fetch clients.');
+            return null;
+        }
+    };
 
     useEffect(() => {
-        const fetchclients = async () => {
-            try {
-                const data = await getClients({ pageNumber: 1, pageQuantity: 5 });
-                setAddresses(data);
-            } catch (error) {
-                console.error('Error fetching clients:', error);
-                //showToastMessage('error', 'Failed to fetch clients.');
-            }
-        };
-
-        if (props.loggedIn) {
-            fetchclients();
-        } else {
+        if (!props.loggedIn) {
             navigate('/login');
         }
     }, [props.loggedIn, navigate]);
 
 
     return(
-        <div className='container flex'>
-            <div className='row'>
-                <DrawerAppBar />
-                <Toolbar />
-            </div>
-
+        <Layout>
             <div className='d-flex flex-row align-items-center mt-3'>
                 <div className='col text-start'>
                     <Typography variant="h3" className='d-flex align-items-center'>
@@ -105,21 +70,18 @@ const Clients = (props) => {
                     </Typography>
                 </div>
                 <div className='col text-end'>
-                    <Button variant="contained" size="large" href="#"> 
+                    <Button variant="contained" size="large" href="/clients/create"> 
                         <AddIcon /> New Address
                     </Button>
                 </div>
             </div>
 
             <div className='container px-0 my-3 d-flex flex-column'>
-                
-
-                <DataTable cols={colAd} rows={addresses} />
+                <DataTableAddress cols={colAddresses} fetchData = {fetchAddresses} deleteEntity = {deleteAddress} />
             </div>
 
-            <ToastContainer />
-        </div>
+        </Layout>
     );
 };
 
-export default Clients;
+export default Addresses;
